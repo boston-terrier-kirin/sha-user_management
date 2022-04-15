70655cfbdcb71117a71e20a4cbb0528e57be4755
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-16 05:40:27 +0900

    Section7-29

---
+++ spring-boot-jwt/src/main/java/com/sha/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.sha.security;
+
+import com.sha.model.User;
+import com.sha.service.UserService;
+import com.sha.utils.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userService.findByUsername(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAuthority(user.getRole().name()));
+
+        return UserPrincipal.builder()
+                .user(user)
+                .id(user.getId())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+}
--- spring-boot-jwt/src/main/java/com/sha/service/UserServiceImpl.java
+++ spring-boot-jwt/src/main/java/com/sha/service/UserServiceImpl.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
-
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 

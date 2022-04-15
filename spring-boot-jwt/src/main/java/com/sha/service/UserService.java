fc3ebabb1258711ecced699452bf48b5f34168b2
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-16 04:46:01 +0900

    Section5-23

---
+++ spring-boot-jwt/src/main/java/com/sha/service/UserService.java
@@ -0,0 +1,17 @@
+package com.sha.service;
+
+import com.sha.model.Role;
+import com.sha.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    User saveUser(User user);
+
+    Optional<User> findByUsername(String username);
+
+    void changeRole(String username, Role role);
+
+    List<User> findAllUsers();
+}
---
+++ spring-boot-jwt/src/main/java/com/sha/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.sha.service;
+
+import com.sha.model.Role;
+import com.sha.model.User;
+import com.sha.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        user.setPassword(this.passwordEncoder.encode(user.getPassword()));
+        user.setRole(Role.USER);
+        user.setCreateTime(LocalDateTime.now());
+
+        return this.userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return this.userRepository.findByUsername(username);
+    }
+
+    @Override
+    @Transactional
+    public void changeRole(String username, Role role) {
+        this.userRepository.updateUserRole(username, role);
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return this.userRepository.findAll();
+    }
+}

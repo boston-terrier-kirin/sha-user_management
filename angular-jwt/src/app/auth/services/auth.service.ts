df2ebf505ad4aa93307fed66b2d2ffebdc7adf37
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-04-16 12:07:54 +0900

    Section16-55

---
+++ angular-jwt/src/app/auth/services/auth.service.ts
@@ -0,0 +1,63 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user.model';
+
+const API_URL = `${environment.base_url}/api/authentication`;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private currentUserSubject = new BehaviorSubject<User | null>(null);
+  currentUser$: Observable<User | null> =
+    this.currentUserSubject.asObservable();
+
+  constructor(private httpClient: HttpClient) {
+    let storageUser;
+    const storageUserAsStr = localStorage.getItem('currentUser');
+    if (storageUserAsStr) {
+      storageUser = JSON.parse(storageUserAsStr);
+    }
+
+    this.currentUserSubject.next(storageUser);
+  }
+
+  get currentUser() {
+    return this.currentUserSubject.value;
+  }
+
+  signin(user: User) {
+    return this.httpClient.post<User>(`${API_URL}/signin`, user).pipe(
+      tap((user) => {
+        if (user) {
+          this.setSessionUser(user);
+        }
+      })
+    );
+  }
+
+  signup(user: User) {
+    return this.httpClient.post<User>(`${API_URL}/signup`, user);
+  }
+
+  signout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+
+  refreshToken() {
+    const token = this.currentUserSubject?.value?.refreshToken;
+    return this.httpClient.post<User>(
+      `${API_URL}/refresh-token?token=${token}`,
+      {}
+    );
+  }
+
+  private setSessionUser(user: User) {
+    localStorage.setItem('currentUser', JSON.stringify(user));
+    this.currentUserSubject.next(user);
+  }
+}
